42ab501625c7845d98bcfba1a70e452c2fd2ef7e
CodrinE <ecodrin@yahoo.com>
2023-03-30 12:15:41 +0300

    Add security groups

---
+++ securitygroups.tf
@@ -0,0 +1,77 @@
+#Create SG for LB. only TCP/80, TCP/443 and outbound access
+resource "aws_security_group" "lb_sg" {
+  provider = aws.master-region
+  name     = "lb_sg"
+  vpc_id   = aws_vpc.vpc_master.id
+  dynamic "ingress" {
+    for_each = var.ingress_rules_lb
+    content {
+      description = ingress.value["description"]
+      from_port   = ingress.value["from_port"]
+      to_port     = ingress.value["to_port"]
+      cidr_blocks = ingress.value["cidr_blocks"]
+      protocol    = ingress.value["protocol"]
+    }
+  }
+  egress {
+    from_port        = 0
+    protocol         = "-1"
+    to_port          = 0
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  tags = {
+    Name = "lb_sg"
+  }
+}
+
+#Create SG for allowing TCp/8080 from * and TCP/22 from your ip in eu-central-1
+resource "aws_security_group" "jenkins_sg" {
+  provider    = aws.master-region
+  name        = "jenkins-sg"
+  description = "Allow TCP/8080 & TCP/22"
+  vpc_id      = aws_vpc.vpc_master.id
+  dynamic "ingress" {
+    for_each = var.ingress_rules_jenkins
+    content {
+      description = ingress.value["description"]
+      from_port   = ingress.value["from_port"]
+      to_port     = ingress.value["to_port"]
+      security_groups = ingress.value["to_port"] == 8080 ? [aws_security_group.lb_sg.id]: null
+      cidr_blocks = ingress.value["cidr_blocks"]
+      protocol    = ingress.value["protocol"]
+    }
+  }
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = {
+    NAME = "jenkins_sg"
+  }
+}
+#create SG for allowing TCP/22 from your IP in eu-west-2
+resource "aws_security_group" "jenkins_sg_london" {
+  provider    = aws.worker-region
+  name        = "jenkins-sg-worker"
+  description = "Allow TCP/8080 & TCP/22"
+  vpc_id      = aws_vpc.vpc_master_london.id
+  dynamic "ingress" {
+    for_each = var.ingress_rules_worker
+    content {
+      description = ingress.value["description"]
+      from_port   = ingress.value["from_port"]
+      to_port     = ingress.value["to_port"]
+      cidr_blocks = ingress.value["cidr_blocks"]
+      protocol    = ingress.value["protocol"]
+    }
+  }
+  egress {
+    from_port   = 0
+    protocol    = "-1"
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
--- variables.tf
+++ variables.tf
@@ -11,3 +11,84 @@ variable "worker-region" {
   type    = string
   default = "eu-west-2"
 }
+
+variable "ingress_rules_lb" {
+  type = list(object({
+    description = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [{
+    description = "Allow https from anywhere"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }, {
+    description = "Allow http from anywhere for redirection"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }]
+  validation {
+    condition = alltrue([
+      for r in var.ingress_rules_lb : r.protocol != "-1"
+    ])
+    error_message = "Protocol can't be all"
+  }
+}
+
+variable "ingress_rules_jenkins" {
+  type = list(object({
+    description = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [{
+    description = "Allow ssh from our public IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }, {
+    description = "Allow anyone on port 8080"
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = null
+    }, {
+    description = "Allow traffic from eu-west-2"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["192.168.1.0/24"]
+  }]
+}
+
+variable "ingress_rules_worker" {
+  type = list(object({
+    description = string
+    from_port   = number
+    to_port     = number
+    protocol    = string
+    cidr_blocks = list(string)
+  }))
+  default = [{
+    description = "Allow 22 from our public IP"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }, {
+    description = "Allow traffic from eu-central-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.1.0/24"]
+  }]
+}
